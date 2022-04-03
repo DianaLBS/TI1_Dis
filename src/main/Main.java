ebdd03cefc0d6a6c27225935163a593da3d2987a
GeorgeU2030 <georgeu2021@gmail.com>
2022-04-03 03:29:15 -0500

    TI_CodeFinished_v1.0

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.Scanner;
 
+import model.HashMap;
 import model.Max_PriorityQueue;
 import model.Min_PriorityQueue;
 
@@ -13,13 +14,76 @@ public class Main {
 		obj.elevatorBuilding();
 	}
 	public void elevatorBuilding() {
-		Max_PriorityQueue<Integer>p=new Max_PriorityQueue(10);
-		Min_PriorityQueue<Integer>q=new Min_PriorityQueue(10);
+		Max_PriorityQueue<Integer>maxQ=new Max_PriorityQueue(10);
+		Min_PriorityQueue<Integer>minQ=new Min_PriorityQueue(10);
 		int numBuilding=sc.nextInt();
 		for(int i=0;i<numBuilding;i++) {
-			int idBuilding=sc.nextInt();
-
+			HashMap<String,Integer>person_office=new HashMap();
+			HashMap<Integer,String>floorUp=new HashMap();
+			HashMap<Integer,String>floorDown=new HashMap();
+			String idBuilding=sc.next();
+            int numPerson=sc.nextInt();
+            int numFloors=sc.nextInt();
+            int office=sc.nextInt();
+            for(int j=0;j<numPerson;j++) {
+            	String name=sc.next();
+            	int actualfloor=sc.nextInt();
+            	int officeB=sc.nextInt();
+            	person_office.put(name,officeB);
+            	int arrivefloor=searchFloorArrive(numFloors,office,officeB);
+            	if(actualfloor<arrivefloor) {
+            		maxQ.add(officeB);
+            		floorUp.put(officeB, name);
+            	}else {
+            		minQ.add(officeB);
+            		floorDown.put(officeB, name);
+            	}
+            }
+            System.out.println();
+            for(int j=0;j<person_office.size();j++) {
+            	System.out.println(person_office.keySet().get(j)+" se mueve a la oficina: "+person_office.values().get(j));
+            }
+            System.out.println();
+            System.out.println("El orden de llegada es ");
+            while(maxQ.peek()!=null) {
+            	for(int j=0;j<floorUp.size();j++) {
+            		if(floorUp.keySet().get(j).equals(maxQ.peek())) {
+            			System.out.println(floorUp.get(maxQ.peek()));
+            		}
+            	}
+            	maxQ.poll();
+            }
+            while(minQ.peek()!=null) {
+            	for(int j=0;j<floorDown.size();j++) {
+            		if(floorDown.keySet().get(j).equals(minQ.peek())) {
+            			System.out.println(floorDown.get(minQ.peek()));
+            			
+            		}
+            	}
+            	minQ.poll();
+            }
+            
 		}
+	}
+	public int searchFloorArrive(int numFloors,int office,int officeB) {
+		int[][]building=new int[numFloors][office];
+		int numberOffice=numFloors*office;
+		int floor=0;
+		for(int i=0;i<numFloors;i++) {
+			for(int j=0;j<office;j++) {
+				building[i][j]=numberOffice;
+				numberOffice--;
+			}
+		}
+		for(int i=0;i<numFloors;i++) {
+			for(int j=0;j<office;j++) {
+             if(building[i][j]==officeB) {
+            	 floor=i+1;
+             }
+
+			}
+		}
+		return floor;
 	}
 
 }
